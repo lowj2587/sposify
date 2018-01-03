c7105c00f16ecf79ab725586caedb8a671c57d3c
Glitch (spotify-web-playback-react) <none>
2018-01-03 01:57:46 +0000

    🚁🎠 Checkpoint src/Spotify/LoginCallback.js:417827 src/App.js:417827 src/Spotify/React.js:417827

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import Header from './layout/Header.js';
 import Footer from './layout/Footer.js';
+import {
+  WebPlaybackScreen as Screen,
+  WebPlayback
+} from './Spotify/React.js';
 import LoginCallback from './Spotify/LoginCallback.js';
 import IntroScreen from './screens/Intro.js';
 import './App.css';
@@ -9,12 +13,29 @@ import './App.css';
 window.onSpotifyWebPlaybackSDKReady = () => {};
 
 export default class App extends Component {
+  state = {
+    deviceId: null,
+    accessToken: null,
+    playerState: null
+  }
+
   componentWillMount() {
-    LoginCallback(this.onAccessTokenExpiration);
+    LoginCallback(
+      this.onSuccessfulAuthorization.bind(this),
+      this.onAccessTokenExpiration.bind(this)
+    );
+  }
+  
+  onSuccessfulAuthorization(access_token) {
+    this.setState({ accessToken: access_token });
   }
   
   onAccessTokenExpiration() {
-    console.log("The access token has expired.");
+    this.setState({
+      accessToken: null,
+      playerState: null
+    });
+    console.error("The access token has expired.");
   }
   
   render() {
@@ -23,7 +44,33 @@ export default class App extends Component {
         <Header />
       
         <main>
-          <IntroScreen />
+          {!this.state.accessToken && <IntroScreen />}
+          {this.state.accessToken &&
+            <WebPlayback
+              playerName="Bilawal's React Player"
+              playerInitialVolume={1.0}
+              playerAutoConnect={true}
+              userAccessToken={this.state.accessToken}
+              onPlayerReady={(data) => console.log("player ready", data)}
+              onPlayerStateChange={(playerState) => this.setState({ playerState: playerState })}>
+
+              <Screen Error>
+                <h3>Error</h3>
+              </Screen>
+
+              <Screen Loading>
+                <h3>Loading Web Playback SDK</h3>
+              </Screen>
+
+              <Screen WaitingForDevice>
+                <h3>Waiting for Device to be Selected</h3>
+              </Screen>
+
+              <Screen Player>
+                <h1>Web Playback SDK + React</h1>
+                {playerState && <NowPlayingView playerState={playerState} />}
+              </Screen>
+            </WebPlayback>}
         </main>
 
         <Footer />
--- src/Spotify/LoginCallback.js
+++ src/Spotify/LoginCallback.js
@@ -1,27 +1,27 @@
-export default (onAccessTokenExpiration) => {
-  let { localStorage, location: {hash} } = window;
+export default (onSuccessfulAuthorization, onAccessTokenExpiration) => {
+  let { location: {hash} } = window;
   let hashExists = hash.length > 0;
-  let hashObj = () => {
-    return hash.substring(1).split('&').reduce((initial, item) => {
-      if (item) {
-        const parts = item.split('=');
-        initial[parts[0]] = decodeURIComponent(parts[1]);
-      }
-      return initial;
-    }, {});
-  };
-  
-  if (!hashExists && localStorage.getItem('access_token')) {
-    return localStorage.getItem('access_token');
-  } else if (hashExists) {
+  let hashObj = hash.substring(1).split('&').reduce((initial, item) => {
+    if (item) {
+      const parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+  if (hashExists) {
     window.location.hash = '';
-    
-    // Set access token
-    localStorage.setItem('access_token', hashObj.access_token);
+        
+    // Let us know it's a successful authorization
+    if (typeof onSuccessfulAuthorization !== "undefined") {
+      onSuccessfulAuthorization(hashObj.access_token);
+    }
 
     // Let us know when the access token expires
     setTimeout(() => {
-      if (typeof onAccessTokenExpiration !== "undefined") { onAccessTokenExpiration(); }
+      if (typeof onAccessTokenExpiration !== "undefined") {
+        onAccessTokenExpiration();
+      }
     }, hashObj.expires_in * 1000);
 
     return hashObj.access_token;
