c563fef768b6e4e0f40bece683431249576c58aa
Glitch (spotify-web-playback-react) <none>
2018-01-04 17:01:40 +0000

    🍛💐 Checkpoint src/Spotify/Login.js:417827 .env:417827

--- src/Spotify/Login.js
+++ src/Spotify/Login.js
@@ -1,7 +1,7 @@
 export default {
   logInWithSpotify: (() => {
-    let client_id      = process.env.CLIENT_ID;
-    let redirect_uri   = process.env.REDIRECT_URI;
+    let client_id      = "bad9e04cf07e4ac89c75a71999f18955";
+    let redirect_uri   = "https://spotify-web-playback-react.glitch.me";
     let scopes         = "streaming user-read-birthdate user-read-email user-read-private user-modify-playback-state";
     let scopes_encoded = scopes.replace(" ", "%20");
 
