a8589a473f9474abf08e10d55e5ca97af1148ae3
Glitch (spotify-web-playback-react) <none>
2018-10-26 20:55:21 +0000

    🎥🍧 Checkpoint ./src/App.css:417827/156 ./public/index.html:417827/12 ./src/App.js:417827/911

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component, Fragment } from 'react';
 
 import './App.css';
-import Header from './layout/Header.js';
-import Footer from './layout/Footer.js';
+import logo from './logo.svg';
 
 export default class App extends Component {  
   render() {
     return (
       <div className="App">
-        <Header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="visually-hidden">Spotify for Developers</span>
+        </header>
+        <div className="secondaryNav secondaryNav-webApi">
+          <h3>Web API Reference <span className="beta">alpha</span></h3>
+        </div>
       
         <main>
           <h3>Loading ...</h3>
         </main>
 
-        <Footer />
+        <footer className="App-footer">
+          <p>&copy; 2018 Spotify AB</p>
+        </footer>
       </div>
     );
   }
